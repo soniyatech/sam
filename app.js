aa42a154fe4a87d4ecedd7e7abb87690fc411a2f
wayne.meng@soniyatechnology.com <wayne.meng@soniyatechnology.com>
2019-07-14 17:01:20 -0700

    multi-page

--- app.js
+++ app.js
@@ -4,10 +4,25 @@ const app = express();
 const hostname = "127.0.0.1";
 const port = 8080;
 
+// set template engine
+app.set("view engine", "ejs");
+
+// set static path
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.send("hello, get reached root.");
+  res.render("index", {});
+});
+
+app.get("/user", (req, res) => {
+  res.render("user/user", {});
+});
+
+app.get("/device", (req, res) => {
+  res.render("device/device", {});
 });
 
 app.listen(port, () => {
-  console.log(`server listening on port ${port}...`);
+  //console.log(`server listening on port ${port}...`);
+  console.log("http server listening at port " + port);
 });
